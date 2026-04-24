c8dcee8f863bbf2e25130ed578d7c08b011d9ab7
Phil <phillip.deeley@student.manchester.ac.uk>
2026-04-24 11:51:17 +0100

    Adjusted file headers and included scripts.js again

---
+++ scripts.js
@@ -0,0 +1,9 @@
+/* ========================================= */
+/* File Name: scripts.js                      */
+/* Project: Phillip Deeley Website            */
+/* Author: Phillip Deeley                     */
+/* Created: 24/04/2026                        */
+/* Last Edited: 24/04/2026                    */
+/* Version: 1.0.0                             */
+/* Notes: Main JavaScript file for website    */
+/* ========================================= */
